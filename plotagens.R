359ab20ba3dc379fc92153693c99d2f76e8980cc
danilosauro <danilodias_lc@hotmail.com>
2024-01-19 16:40:02 -0300

    limpeza e tratamento dos scripts + analises individualizadas

--- plotagens.R
+++ plotagens.R
@@ -1,15 +1,16 @@
-### plotagens inerentes aos estados 
+### plotagens inerentes aos estados aids
 
 ggplot(piaui, aes(x = ano, y = QUANTIDADE_CASOS_AIDS, fill = UF)) +
   geom_bar(stat = "identity", position = "dodge") +
-  scale_fill_manual(values = paleta_cores) +
+  scale_fill_manual(values = 'grey') +
   labs(title = "Quantidade de Casos de AIDS por ano no estado",
        x = "Ano",
        y = "Quantidade de Casos") +
   theme_minimal() + 
   theme(legend.position = "right")
 
-### gráfico da quantidade de casos de aids por estado em cada ano 
+### gráfico da quantidade de casos de aids por estado em cada ano
+
 paleta_cores <- brewer.pal(n = 9, name = "Set1")
 ggplot(painel_data, aes(x = ano, y = QUANTIDADE_CASOS_AIDS, fill = UF)) +
   geom_bar(stat = "identity", position = "dodge") +
@@ -23,6 +24,7 @@ ggplot(painel_data, aes(x = ano, y = QUANTIDADE_CASOS_AIDS, fill = UF)) +
 ggsave("aids_estado_ano.jpg", width = 10, height = 7)
 
 ### gráfico da quantidade de casos de sifilis por estado em cada ano
+
 paleta_cores <- brewer.pal(n = 9, name = "Set1") 
 ggplot(painel_data, aes(x = ano, y = QUANTIDADE_CASOS_SIFILIS, fill = UF)) +
   geom_bar(stat = "identity", position = "dodge") +
@@ -46,16 +48,17 @@ ggplot(alagoas, aes(x = MEDIA_ANOS_ESTUDO, y = RENDA_PER_CAPITA)) +
 
 
 #### avaliações individualizadas por estado --- escolaridade ~ SIFILIS
-ggplot(alagoas, aes(x = MEDIA_ANOS_ESTUDO, y = QUANTIDADE_CASOS_SIFILIS)) +
+ggplot(bahia, aes(x = MEDIA_ANOS_ESTUDO, y = QUANTIDADE_CASOS_SIFILIS)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE, color = "red") +
   labs(title = "Relação entre média de anos de estudo e casos de Sífilis adquirida",
        x = "Escolaridade média",
-       y = "Notificações referentes a Sífilis adquirida") 
+       y = "Notificações referentes a Sífilis adquirida")  
+
 
 ### avaliações invidualizadas por estado --- Renda per capita ~  SIFILIS 
 
-ggplot(alagoas, aes(x = RENDA_PER_CAPITA, y = QUANTIDADE_CASOS_SIFILIS)) +
+ggplot(ceara, aes(x = RENDA_PER_CAPITA, y = QUANTIDADE_CASOS_SIFILIS)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE, color = "red") +
   labs(title = "Relação entre Renda per capita e casos de Sífilis adquirida",
@@ -103,7 +106,34 @@ ggplot(alagoas, aes(x = RENDA_PER_CAPITA, y = QUANTIDADE_CASOS_AIDS)) +
   geom_smooth(method = "lm", se = FALSE, color = "red") +
   labs(title = "Relação entre Renda per capita e casos de HIV_Aids",
        x = "Renda per capita",
-       y = "Notificações referentes a HIV_Aids") 
+       y = "Notificações referentes a HIV_Aids")
+
+### avaliações generalizadas painel_data 
+
+ggplot(painel_data, aes(x = MEDIA_ANOS_ESTUDO, y = QUANTIDADE_CASOS_SIFILIS)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE, color = "red") +
+  labs(title = "Relação entre média de anos de estudo e casos notificados de Sífilis adquirida",
+       x = "Escolaridade média",
+       y = "Notificações referentes a Sífilis adquirida") 
+
+
+ggplot(painel_data, aes(x = RENDA_PER_CAPITA, y = QUANTIDADE_CASOS_SIFILIS)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE, color = "red") +
+  labs(title = "Relação entre Renda per capita e casos notificados de Sífilis adquirida",
+       x = "Renda per capita",
+       y = "Notificações referentes a Sífilis adquirida") 
+
+ggplot(painel_data, aes(x = ESPERANCA_VIDA_NASC, y = QUANTIDADE_CASOS_SIFILIS)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE, color = "red") +
+  labs(title = "Relação entre Esperança de vida ao nascer e casos notificados de Sífilis adquirida",
+       x = "Esperança de vida ao nascer",
+       y = "Notificações referentes a Sífilis adquirida")
+
+
+
 
 ## tabelas
 painel_data %>%
@@ -111,4 +141,71 @@ painel_data %>%
   modify_header(label = "**Variable**")
 
 painel_data %>%
-  tbl_summary(UF)
+  tbl_summary(UF) 
+
+
+# plots resíduos modelo generalizado (toda a região nordeste) 
+library(ggplot2)
+library(broom)
+
+residuos_sifilis <- residuals(modelo_sifilis)
+
+dados_residuos_sifilis <- data.frame(RENDA_PER_CAPITA = painel_data$RENDA_PER_CAPITA,
+                                     Residuos = residuos_sifilis)
+
+ggplot(dados_residuos_sifilis, aes(x = RENDA_PER_CAPITA, y = Residuos)) +
+  geom_point() +
+  geom_smooth(method = "loess") +
+  labs(title = "Dispersão entre Renda Per Capita e Resíduos da Sífilis",
+       x = "Renda Per Capita",
+       y = "Resíduos")
+
+
+######## ------ PLOTAGENS DOS RESÍDUOS PELOS DETERMINANTES SOCIAIS -------- ###########
+
+resultados <- list(
+  Alagoas = resultados_por_estado$Alagoas,
+  Bahia = resultados_por_estado$Bahia,
+  Ceará = resultados_por_estado$Ceará,
+  Maranhão = resultados_por_estado$Maranhão,
+  Paraíba = resultados_por_estado$Paraíba,
+  Pernambuco = resultados_por_estado$Pernambuco,
+  Piauí = resultados_por_estado$Piauí,
+  `Rio Grande do Norte` = resultados_por_estado$`Rio Grande do Norte`,
+  Sergipe = resultados_por_estado$Sergipe
+)
+
+
+for (estado in names(resultados_por_estado)) {
+  
+  dados_aumentados <- augment(resultados_por_estado[[estado]])
+  
+  
+  ggplot(dados_aumentados, aes(x = RENDA_PER_CAPITA, y = .resid)) +
+    geom_point() +
+    geom_smooth(method = "lm", se = FALSE, col = "red") +
+    labs(title = paste("Dispersão entre Renda Per Capita e Resíduos -", estado),
+         x = "Renda Per Capita",
+         y = "Resíduos")
+  
+  ggsave(paste("/GRAFICOS/rpc_x_residuos_estado", estado, ".png", sep = ""), plot = last_plot())
+  
+  ggplot(dados_aumentados, aes(x= MEDIA_ANOS_ESTUDO, y= .resid)) + 
+    geom_point()+
+    geom_smooth(method = "lm", se = FALSE, col = "red") +
+    labs(title = paste("Dispersão entre Escolaridade média e Resíduos -", estado),
+         x = "Escolaridade média",
+         y = "Resíduos")
+  
+  ggsave(paste("/GRAFICOS/escolaridade_media_x_residuos_estado", estado, ".png", sep = ""), plot = last_plot())
+  
+  ggplot(dados_aumentados, aes(x= ESPERANCA_VIDA_NASC, y= .resid)) + 
+    geom_point()+
+    geom_smooth(method = "lm", se = FALSE, col = "red") +
+    labs(title = paste("Dispersão entre Esperança de vida ao nascer e Resíduos -", estado),
+         x = "Esperança de vida ao nascer",
+         y = "Resíduos")
+  
+  ggsave(paste("/GRAFICOS/esperanca_vida_nasc_x_residuos_estado", estado, ".png", sep = ""), plot = last_plot())
+  
+}
--- testes_estatisticos.R
+++ testes_estatisticos.R
@@ -1,18 +1,50 @@
 ## avaliações em painel
 
-## modelos linear  ~ glm 
-model <- lm(QUANTIDADE_CASOS_AIDS ~ MEDIA_ANOS_ESTUDO, data = alagoas) 
-summary(model)
 
-# modelo linear múltiplo  ~ glm 
-modelo_regressao_multipla_interacoes <- lm(QUANTIDADE_CASOS_AIDS ~  MEDIA_ANOS_ESTUDO  + RENDA_PER_CAPITA + ESPERANCA_VIDA_NASC, data = maranhao)
-summary(modelo_regressao_multipla_interacoes) 
+## modelos linear plm por estado 
 
-modelo_regressao_multipla_interacoes_all <- lm(QUANTIDADE_CASOS_SIFILIS ~ RENDA_PER_CAPITA * UF + MEDIA_ANOS_ESTUDO * UF + ESPERANCA_VIDA_NASC * UF, data = painel_data)
-summary(modelo_regressao_multipla_interacoes_all) 
+lista_estados <- list(alagoas, bahia, ceara, maranhao, paraiba, pernambuco, piaui, rn, sergipe)
+
+## aids 
+
+generate_panel_data_model_aids <- function(estado){
+  for(i in lista_estados){
+    modelo_estado <- plm(QUANTIDADE_CASOS_AIDS ~ RENDA_PER_CAPITA + MEDIA_ANOS_ESTUDO + ESPERANCA_VIDA_NASC, data = estado, model = 'within')
+  }
+  return(summary(modelo_estado))
+} 
+
+generate_panel_data_model_aids(alagoas) 
+generate_panel_data_model_aids(bahia)
+generate_panel_data_model_aids(ceara) 
+generate_panel_data_model_aids(maranhao)
+generate_panel_data_model_aids(paraiba)
+generate_panel_data__model_aids(piaui)
+generate_panel_data__model_aids(pernambuco)
+generate_panel_data_model_aids(rn)
+generate_panel_data__model_aids(sergipe)
+
+## sifilis 
+
+generate_panel_data_model_sifilis <- function(estado){
+  for(i in lista_estados){
+    modelo_estado <- plm(QUANTIDADE_CASOS_SIFILIS ~ RENDA_PER_CAPITA + MEDIA_ANOS_ESTUDO + ESPERANCA_VIDA_NASC, data = estado, model = 'within')
+  }
+  return(summary(modelo_estado))
+} 
+
+generate_panel_data_model_sifilis(alagoas) 
+generate_panel_data_model_sifilis(bahia)
+generate_panel_data_model_sifilis(ceara) 
+generate_panel_data_model_sifilis(maranhao)
+generate_panel_data_model_sifilis(paraiba)
+generate_panel_data_model_sifilis(piaui)
+generate_panel_data_model_sifilis(pernambuco)
+generate_panel_data_model_sifilis(rn)
+generate_panel_data_model_sifilis(sergipe)
 
 
-# Ajuste modelo de regressão por dados em painel por efeitos fixos
+# Ajuste modelo de regressão por dados em painel por efeitos fixos para o dataset completo
 
 modelo_sifilis <- plm(QUANTIDADE_CASOS_SIFILIS ~ RENDA_PER_CAPITA + MEDIA_ANOS_ESTUDO + ESPERANCA_VIDA_NASC, data = painel_data, model="within")
 summary(modelo_sifilis) 
@@ -24,7 +56,3 @@ modelo_aids <- plm(QUANTIDADE_CASOS_AIDS ~ RENDA_PER_CAPITA + MEDIA_ANOS_ESTUDO 
 summary(modelo_aids) 
 
 summary(fixef(modelo_sifilis))
-
-
-## avaliações individualizadas para os estados 
-
